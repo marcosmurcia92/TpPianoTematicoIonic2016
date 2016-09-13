36278e3e405d889e610a112d0016dbf1eae723cf
Marcos Facundo Murcia <marcosmurcia92@gmail.com>
2016-09-13 01:47:18 -0300

    Cambiado el plugin NgCordovaMedia por NgCordovaNativeAudio. Performance de Sonidos incrementada y no presentan ningun problema de reproduccion.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal,$ionicPlatform, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal,$cordovaNativeAudio,$ionicPlatform, $timeout) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -8,6 +8,140 @@ angular.module('starter.controllers', [])
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+
+  $ionicPlatform.ready(function(){
+        //ANIMALES
+      $cordovaNativeAudio
+        .preloadSimple('cat', 'mp3/animales/cat.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('dog', 'mp3/animales/dog.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('duck', 'mp3/animales/duck.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('horse', 'mp3/animales/horse.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('sheep', 'mp3/animales/sheep.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('turkey', 'mp3/animales/turkey.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+
+        //BANDA
+      $cordovaNativeAudio
+        .preloadSimple('drum', 'mp3/banda/drum.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('flute', 'mp3/banda/flute.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('guitar', 'mp3/banda/guitar.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('trumpet', 'mp3/banda/trumpet.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('violin', 'mp3/banda/violin.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('xylo', 'mp3/banda/xylo.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+
+        //COMIC
+      $cordovaNativeAudio
+        .preloadSimple('appear', 'mp3/comic/appear.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('arrow', 'mp3/comic/arrow.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('cuckoo', 'mp3/comic/cuckoo.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('flee', 'mp3/comic/flee.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('jump', 'mp3/comic/jump.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+      $cordovaNativeAudio
+        .preloadSimple('slip', 'mp3/comic/slip.mp3')
+        .then(function (msg) {
+          console.log(msg);
+        }, function (error) {
+          alert(error);
+        });
+  });
 
   // Form data for the login modal
   $scope.loginData = {};
@@ -47,21 +181,21 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('PianoCtrl', function($scope, $stateParams,$cordovaVibration,$cordovaMedia,$cordovaDevice,$ionicSideMenuDelegate) {
+.controller('PianoCtrl', function($scope, $stateParams,$timeout,$cordovaVibration,$cordovaNativeAudio,$cordovaDevice,$ionicSideMenuDelegate) {
   $ionicSideMenuDelegate.canDragContent(false);
-
 
   $scope.TocarPiano=function(carpeta,sonido){
       console.log("/android_asset/www/mp3/"+carpeta+"/"+sonido+".mp3");
-    var src = "";
+    //var src = "";
       $cordovaVibration.vibrate(500);
-      src = "/android_asset/www/mp3/"+carpeta+"/"+sonido+".mp3";
-      var media = $cordovaMedia.newMedia(src);
-      media.setVolume(1);
-      media.play();
+      $cordovaNativeAudio.play(sonido);
+      // src = "/android_asset/www/mp3/"+carpeta+"/"+sonido+".mp3";
+      // var media = $cordovaMedia.newMedia(src);
+      // media.setVolume(1);
+      // media.play();
 
-      $timeout(function(){
-        media.release();
-      },1000);
+      // $timeout(function(){
+      //   media.release();
+      // },1000);
   };
 });
